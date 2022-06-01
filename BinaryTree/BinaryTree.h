efa9af77c2f2f30c8a3e8435862bf11cfa4cd35d
yeonggwangchoi <dudrhkd7811@naver.com>
2022-06-01 09:15:34 +0900

    220601 commit

---
+++ BinaryTree/BinaryTree.h
@@ -0,0 +1,21 @@
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
+
+typedef int BTData;
+
+typedef struct _bTreeeNode
+{
+    BTData data;
+    struct _bTreeNode* left;
+    struct _bTreeNode* right;
+}BTreeNode;
+
+BTreeNode* MakeBTreeNode(void);
+BTData GetData(BTreeNode* bt);
+void SetData(BTreeNode* bt, BTData data);
+BTreeNode* GetLeftSubTree(BTreeNode* bt);
+BTreeNode* GetRightSubTree(BTreeNode* bt);
+void MakeLeftSubTree(BTreeNode* main, BTreeNode* sub);
+void MakeRightSubTree(BTreeNode* main, BTreeNode* sub);
+
+#endif // !BINARY_TREE_H
